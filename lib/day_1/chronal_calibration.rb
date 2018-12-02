2a64602dece80536ff2834cb96f83c4211932d90
Anderson Fernandes <fernandesanderson14@gmail.com>
2018-12-02 15:17:28 -0300

    Added day 1 part 2
    - changed specs
    - added new input files
    - changed chronal_calibration implementation
    
    Signed-off-by: Anderson Fernandes <fernandesanderson14@gmail.com>

--- lib/day_1/chronal_calibration.rb
+++ lib/day_1/chronal_calibration.rb
@@ -1,20 +1,48 @@
+# frozen_string_literal: true
+
 class ChronalCalibration
-  attr_reader :input_filename
+  attr_reader :input_filename, :frequencies
 
   def initialize(input_filename)
     @input_filename = input_filename
+    @frequencies = [0]
   end
 
   def call
     return unless input_filename
 
-    current_value = 0
-    File.open(input_filename, 'r') do |file|
-      while line = file.gets
-        current_value += line.to_i
+    current_frequency = 0
+    frequency_not_found = true
+    while frequency_not_found
+      File.open(input_filename, 'r') do |file|
+        while (line = file.gets) && frequency_not_found
+          current_frequency += line.to_i
+          frequency_not_found = false if frequency_exists?(current_frequency)
+
+          process_frequency(current_frequency)
+        end
       end
     end
 
-    current_value
+    current_frequency
+  end
+
+  private
+
+  def frequency_exists?(frequency)
+    return if frequencies[frequency].nil?
+
+    return true if frequency.zero?
+    return frequencies[frequency.abs]&.fetch(:-, false) if frequency.negative?
+    return frequencies[frequency.abs]&.fetch(:+, false) if frequency.positive?
+  end
+
+  def process_frequency(frequency)
+    return if frequency.zero?
+
+    frequencies[frequency.abs] ||= {}
+
+    frequencies[frequency.abs][:-] = true if frequency.negative?
+    frequencies[frequency.abs][:+] = true if frequency.positive?
   end
 end
--- spec/day_1/chronal_calibration_spec.rb
+++ spec/day_1/chronal_calibration_spec.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../../lib/day_1/chronal_calibration'
 
 describe ChronalCalibration do
@@ -5,24 +7,31 @@ describe ChronalCalibration do
   subject { described_class.new input_file }
   let(:result) { subject.call }
 
-  context 'When the input is +1, +1, +1' do
+  context 'When the input is +1, -1' do
     let(:input_file) { 'spec/support/day_1/input_01' }
-    it 'the result should be 3' do
-      expect(result).to be == 3
-    end
-  end
-
-  context 'When the input is +1, +1, -2' do
-    let(:input_file) { 'spec/support/day_1/input_02' }
     it 'the result should be 0' do
       expect(result).to be == 0
     end
   end
 
-  context 'When the input is -1, -2, -3' do
+  context 'When the input is +3, +3, +4, -2, -4' do
+    let(:input_file) { 'spec/support/day_1/input_02' }
+    it 'the result should be 10' do
+      expect(result).to be == 10
+    end
+  end
+
+  context 'When the input is -6, +3, +8, +5, -6' do
     let(:input_file) { 'spec/support/day_1/input_03' }
-    it 'the result should be -6' do
-      expect(result).to be == -6
+    it 'the result should be 5' do
+      expect(result).to be == 5
+    end
+  end
+
+  context 'When the input is +7, +7, -2, -7, -4' do
+    let(:input_file) { 'spec/support/day_1/input_04' }
+    it 'the result should be 14' do
+      expect(result).to be == 14
     end
   end
 end
